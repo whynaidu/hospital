83f15aef03288e242233461b92dd0b161b6a2bde
priya8425 <yadavpriya1425@gmail.com>
2022-05-30 10:28:12 +0530

    s

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="index.php" class="brand-link" style="text-decoration:none;>
+  <a href="dashboard.php" class="brand-link" style="text-decoration:none;>
     <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
       style="opacity: .8;">
     <span class="brand-text font-weight-light">Hospital</span>
